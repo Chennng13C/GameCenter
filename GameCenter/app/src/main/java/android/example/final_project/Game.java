21ac1373233cdddfd3eb9a494f520367786b2c8b
Chennng13C <t108360130@ntut.org.tw>
2021-12-20 20:32:20 +0800

    chennng

--- GameCenter/app/src/main/java/android/example/final_project/Game.java
+++ GameCenter/app/src/main/java/android/example/final_project/Game.java
@@ -1,5 +1,22 @@
 package android.example.final_project;
 
 public class Game{
-
+    public String name;
+    public String description;
+    public String image;
+    public String ingredient;
+    public String Date;
+    public String Developer;
+    public String Publisher;
+    public String procedure;
+    public Game(String name, String description, String image, String ingredients, String procedure,String Date,String Developer, String Publisher) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.ingredient = ingredients;
+        this.Date=Date;
+        this.Developer=Developer;
+        this.Publisher=Publisher;
+        this.procedure = procedure;
+    }
 }
